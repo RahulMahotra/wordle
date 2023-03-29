c21e8e13e66395c6e1401a1287066e9551a7b82a
RahulMahotra <54627917+RahulMahotra@users.noreply.github.com>
2023-03-29 12:34:45 +0530

    Update useWordle.js

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -3,28 +3,24 @@ import { useState } from 'react'
 const useWordle = (solution) => {
   const [turn, setTurn] = useState(0) 
   const [currentGuess, setCurrentGuess] = useState('')
-  const [guesses, setGuesses] = useState([...Array(6)]) // each guess is an array
-  const [history, setHistory] = useState([]) // each guess is a string
+  const [guesses, setGuesses] = useState([...Array(6)])
+  const [history, setHistory] = useState([])
   const [isCorrect, setIsCorrect] = useState(false)
-  const [usedKeys, setUsedKeys] = useState({}) // {a: 'grey', b: 'green', c: 'yellow'} etc
+  const [usedKeys, setUsedKeys] = useState({})
 
-  // format a guess into an array of letter objects 
-  // e.g. [{key: 'a', color: 'yellow'}]
   const formatGuess = () => {
     let solutionArray = [...solution]
     let formattedGuess = [...currentGuess].map((l) => {
       return {key: l, color: 'grey'}
     })
 
-    // find any green letters
     formattedGuess.forEach((l, i) => {
       if (solution[i] === l.key) {
         formattedGuess[i].color = 'green'
         solutionArray[i] = null
       }
     })
-    
-    // find any yellow letters
+
     formattedGuess.forEach((l, i) => {
       if (solutionArray.includes(l.key) && l.color !== 'green') {
         formattedGuess[i].color = 'yellow'
@@ -34,10 +30,7 @@ const useWordle = (solution) => {
 
     return formattedGuess
   }
-
-  // add a new guess to the guesses state
-  // update the isCorrect state if the guess is correct
-  // add one to the turn state
+  
   const addNewGuess = (formattedGuess) => {
     if (currentGuess === solution) {
       setIsCorrect(true)
@@ -75,22 +68,20 @@ const useWordle = (solution) => {
     })
     setCurrentGuess('')
   }
-
-  // handle keyup event & track current guess
-  // if user presses enter, add the new guess
+  
   const handleKeyup = ({ key }) => {
     if (key === 'Enter') {
-      // only add guess if turn is less than 5
+
       if (turn > 5) {
         console.log('you used all your guesses!')
         return
       }
-      // do not allow duplicate words
+
       if (history.includes(currentGuess)) {
         console.log('you already tried that word.')
         return
       }
-      // check word is 5 chars
+
       if (currentGuess.length !== 5) {
         console.log('word must be 5 chars.')
         return
